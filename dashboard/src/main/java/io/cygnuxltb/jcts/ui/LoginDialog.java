1a5b3b970240288360beed3a6cf0287d3c4412c5
Yellow013 <wk9988@gmail.com>
2023-07-16 21:37:53 +0900

    update dashboard

--- dashboard/src/main/java/io/cygnuxltb/jcts/ui/LoginDialog.java
+++ dashboard/src/main/java/io/cygnuxltb/jcts/ui/LoginDialog.java
@@ -7,6 +7,7 @@ import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.KeyStroke;
+import java.awt.AWTEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
@@ -24,36 +25,36 @@ public class LoginDialog extends JDialog {
         setModal(true);
         getRootPane().setDefaultButton(buttonOK);
 
-        buttonOK.addActionListener(e -> onLogin());
-        buttonCancel.addActionListener(e -> onClose());
+        buttonOK.addActionListener(this::onLogin);
+        buttonCancel.addActionListener(this::onClose);
 
         // call onCancel() when cross is clicked
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {
-                onClose();
+                onClose(e);
             }
         });
 
         // call onLogin() on ENTER
-        contentPane.registerKeyboardAction(e -> onLogin(),
+        contentPane.registerKeyboardAction(this::onLogin,
                 KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0),
                 JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
         );
 
         // call onClose() on ESCAPE
-        contentPane.registerKeyboardAction(e -> onClose(),
+        contentPane.registerKeyboardAction(this::onClose,
                 KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
                 JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
     }
 
-    private void onLogin() {
+    private void onLogin(AWTEvent event) {
         // add your code here
         System.out.println("onLogin");
         dispose();
     }
 
-    private void onClose() {
+    private void onClose(AWTEvent event) {
         // add your code here if necessary
         System.out.println("onClose");
         dispose();
--- dashboard/src/main/java/io/cygnuxltb/jcts/ui/MainWindow.java
+++ dashboard/src/main/java/io/cygnuxltb/jcts/ui/MainWindow.java
@@ -1,2 +1,14 @@
-package io.cygnuxltb.jcts.ui;public class MainWindow {
+package io.cygnuxltb.jcts.ui;
+
+import javax.swing.JButton;
+
+public class MainWindow {
+    private JButton button1;
+    private JButton button2;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
 }
