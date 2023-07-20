dc04de74e03edaae7b1c63f11abc1f278434664b
Yellow013 <wk9988@gmail.com>
2023-07-20 23:50:24 +0900

    update dashboard

--- dashboard/src/main/java/io/cygnuxltb/jcts/ui/MainWindow.java
+++ dashboard/src/main/java/io/cygnuxltb/jcts/ui/MainWindow.java
@@ -1,14 +1,16 @@
 package io.cygnuxltb.jcts.ui;
 
 import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class MainWindow {
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton button4;
+    private JButton startButton;
+    private JButton stopButton;
+    private JButton disconnectButton;
+    private JButton resetButton;
     private JButton button5;
     private JButton button6;
     private JButton button7;
     private JButton button8;
+    private JPanel mainPanel;
 }
--- dashboard/src/main/java/io/cygnuxltb/jcts/ui/SwingApp.java
+++ dashboard/src/main/java/io/cygnuxltb/jcts/ui/SwingApp.java
@@ -1,7 +1,10 @@
 package io.cygnuxltb.jcts.ui;
 
-import javax.swing.*;
-import java.awt.*;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
