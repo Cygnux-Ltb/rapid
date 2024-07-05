d099427b9074a20065958e23fdeb0d7566bc8639
yellow013 <wk9988@gmail.com>
2024-07-05 21:56:48 +0900

    update adaptor ctp service

---
+++ adaptor-ctp-service/src/main/java/io/rapid/adaptor/ctp/CtpBootApplication.java
@@ -0,0 +1,23 @@
+package io.rapid.adaptor.ctp;
+
+import io.mercury.common.log4j2.Log4j2Configurator;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static io.mercury.common.log4j2.Log4j2Configurator.LogLevel.INFO;
+
+@SpringBootApplication
+public class CtpBootApplication {
+
+    static {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>[CTP ADAPTOR START]<<<<<<<<<<<<<<<<<<<<<");
+        Log4j2Configurator.setLogFolder("rapid");
+        Log4j2Configurator.setLogFilename("adaptor-ctp");
+        Log4j2Configurator.setLogLevel(INFO);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtpBootApplication.class, args);
+    }
+
+}
---
+++ adaptor-ctp-service/src/main/java/io/rapid/adaptor/ctp/launch/CtpZmqHandler.java
@@ -0,0 +1,50 @@
+package io.rapid.adaptor.ctp.launch;
+
+import com.typesafe.config.Config;
+import io.rapid.adaptor.ctp.gateway.event.FtdcEvent;
+import io.mercury.common.collections.queue.Queue;
+import io.mercury.common.concurrent.queue.ScQueueWithJCT;
+import io.mercury.common.functional.Handler;
+import io.mercury.common.log4j2.Log4j2LoggerFactory;
+import io.mercury.common.thread.RunnableComponent.StartMode;
+import io.mercury.serialization.json.JsonWrapper;
+import io.mercury.transport.zmq.ZmqConfigurator;
+import io.mercury.transport.zmq.ZmqPublisher;
+import org.slf4j.Logger;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class CtpZmqHandler implements Closeable, Handler<FtdcEvent> {
+
+    private static final Logger log = Log4j2LoggerFactory.getLogger(CtpZmqHandler.class);
+
+    private final ZmqPublisher<String> publisher;
+
+    private final Queue<FtdcEvent> queue;
+
+    public CtpZmqHandler(Config config) {
+        String topic = config.getString("zmq.topic");
+        log.info("config zmq.topic == [{}]", topic);
+        this.publisher = ZmqConfigurator.withConfig(config).newPublisherWithString(topic);
+        this.queue = ScQueueWithJCT.mpscQueue("CtpZmqHandler-Queue").capacity(32)
+                .startMode(StartMode.auto()).process(msg -> {
+                    String json = JsonWrapper.toJson(msg);
+                    log.info("Received msg -> {}", json);
+                    publisher.publish(json);
+                });
+    }
+
+    @Override
+    public void close() throws IOException {
+        while (!queue.isEmpty())
+            ;
+        publisher.close();
+    }
+
+    @Override
+    public void handle(FtdcEvent msg) {
+        queue.enqueue(msg);
+    }
+
+}
