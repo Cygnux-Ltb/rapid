316880dcc1b6d49dc2869621f15e681a1de70987
yellow013 <wk9988@gmail.com>
2025-02-04 22:35:40 +0900

    adaptor test

--- adaptor-fix/src/test/java/io/rapid/adaptor/fix/AppTest.java
+++ adaptor-fix/src/test/java/io/rapid/adaptor/fix/AppTest.java
@@ -1,8 +1,8 @@
 package io.rapid.adaptor.fix;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
